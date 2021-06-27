8fbfcbeae6fb27397a927c561cea15484f52657a
Dlean Jeans <12462089+DleanJeans@users.noreply.github.com>
2021-06-27 13:43:07 +0700

    Emojis: Prioritize our servers

--- cogs/common/converter/emojis.py
+++ cogs/common/converter/emojis.py
@@ -1,18 +1,24 @@
 import discord
+import env
 
 from discord.ext import commands
+
 
 class NitroEmojiConverter(commands.PartialEmojiConverter):
     async def convert(self, ctx, arg):
         no_colon = arg.replace(':', '')
         emoji = get_known_emoji(ctx.bot.emojis, no_colon, ctx.author)
         if not emoji:
-            try: emoji = await super().convert(ctx, arg)
-            except: emoji = no_colon
+            try:
+                emoji = await super().convert(ctx, arg)
+            except:
+                emoji = no_colon
         return emoji
+
 
 def get_known_emoji(emojis, name, user=None):
     def score_relevance(emoji):
+        total_score = 0
         try:
             in_guild = emoji.guild in user.mutual_guilds
             total_score = in_guild * 100
@@ -21,15 +27,18 @@ def get_known_emoji(emojis, name, user=None):
                 sum_roles = sum([r.position for r in member.roles])
                 is_owner = emoji.guild.owner == member
                 total_score += sum_roles + is_owner * 100
-            return total_score
         except:
-            return 0
+            pass
+        from_jenna_guilds = emoji.guild.id in [
+            env.HOME_GUILD, env.EMOTES_BUFFER_GUILD
+        ]
+        total_score += int(from_jenna_guilds)
+        return total_score
 
     duplicates = [e for e in emojis if e.name == name]
     duplicates.sort(key=score_relevance, reverse=True)
     return duplicates[0] if duplicates else None
-    return discord.utils.get(emojis, name=name)
 
-emoji_converter = NitroEmojiConverter()
+
 async def convert(ctx, arg):
-    return await emoji_converter.convert(ctx, arg)
+    return await NitroEmojiConverter().convert(ctx, arg)
