bbe82fc858a789509106908e53853f33f073c040
Dlean Jeans <12462089+DleanJeans@users.noreply.github.com>
2021-07-11 23:28:38 +0700

    Update pytest + coverage configs

--- coverage.bat
+++ coverage.bat
@@ -1,4 +1,7 @@
 @echo off
 py -m coverage run --source=. -m pytest
 py -m coverage report
-IF "%1"=="html" (py -m coverage html & start htmlcov/index.html)
+IF "%1"=="html" py -m coverage html
+CHOICE /N /C YN /M "Do you wanna open the coverage report website? (Y/N)"%2
+IF ERRORLEVEL==2 GOTO :EOF
+start htmlcov/index.html
