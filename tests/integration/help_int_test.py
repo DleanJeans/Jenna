797b3953646aef74b6fc5ee865c87799ff566858
Dlean Jeans <12462089+DleanJeans@users.noreply.github.com>
2021-07-12 10:32:14 +0700

    Simplify tests for Help + Jishaku

--- tests/integration/help_int_test.py
+++ tests/integration/help_int_test.py
@@ -1,14 +1,14 @@
-from discord.ext.test.runner import get_embed
 import pytest
-from conftest import verify_message
 from discord.ext import commands
-from discord.ext.test import message, verify
+from discord.ext.test import message, get_embed
+
 
 @pytest.fixture
 def cog_list():
     return ['help']
 
+
 @pytest.mark.asyncio
-async def test_show_help_on_mention(bot:commands.Bot):
+async def test_show_help_on_mention(bot: commands.Bot):
     await message(f'{bot.user.mention}')
-    assert verify().message().embed()
+    assert get_embed()
--- tests/integration/jishaku_int_test.py
+++ tests/integration/jishaku_int_test.py
@@ -1,4 +1,5 @@
 import discord.ext.test as dpytest
+from discord.ext.test.runner import get_message
 from jenna import Jenna
 from discord.ext import commands
 import pytest
@@ -12,24 +13,17 @@ def external_cogs():
 
 
 @pytest.fixture(autouse=True)
-def setup_owner(bot, members):
-    bot.owner_id = members[0].id
+def setup_owner(mocker):
+    mocker.patch.object(Jenna, 'is_owner', return_value=True)
 
 
 def verify_jsk():
     verify_message(contains='Module was loaded')
 
 
-
-
-@pytest.fixture
-def disable_startup_notif(mocker):
-    mocker.patch.object(Jenna, 'notify_owner')
-
-
-
 @pytest.mark.asyncio
-async def test_custom_alias_sk(bot: Jenna, disable_startup_notif):
-    await bot.on_ready()
+@pytest.mark.slow
+async def test_custom_alias_sk(bot: Jenna):
+    bot.add_sk_alias_for_jishaku()
     await send_cmd('sk')
     verify_jsk()
