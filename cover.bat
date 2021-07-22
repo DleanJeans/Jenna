cc5f65deb7f2f5e1d9d41b670d13764d6b274655
Dlean Jeans <12462089+DleanJeans@users.noreply.github.com>
2021-07-22 22:44:36 +0700

    Update gitignore + coverage batch

---
+++ cover.bat
@@ -0,0 +1,29 @@
+@echo off
+
+set slow_tests=enable_socket or slow
+if "%1"=="fast" (
+    set marker=-m "not (%slow_tests%)"
+) else (
+if "%1"=="slow" (
+    set marker=-m "%slow_tests%"
+))
+
+set args=
+
+:remove_first_arg
+shift
+if "%1"=="" goto run_test
+set args=%args% %1
+goto remove_first_arg
+
+:run_test
+py -m pytest ^
+    --durations=5 ^
+    --cov=. ^
+    --cov-report xml:cov/cov.xml ^
+    --cov-report html:cov/html/ ^
+    %marker% %args%
+
+choice /N /C YN /M "Open report page? (Y/N)"
+if ERRORLEVEL==2 goto :eof
+start cov/html/index.html
--- coverage.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-py -m coverage run --source=. -m pytest
-py -m coverage report
-IF "%1"=="html" py -m coverage html
-CHOICE /N /C YN /M "Do you wanna open the coverage report website? (Y/N)"%2
-IF ERRORLEVEL==2 GOTO :EOF
-start htmlcov/index.html
